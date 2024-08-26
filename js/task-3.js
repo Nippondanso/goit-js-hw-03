481fa85a6ab3e7bc1ce38632d90a923b34d083a2
Maksym Kopaiev <baton-opuses-0e@icloud.com>
2024-08-26 20:37:57 +0300

    hw 3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,20 @@
-function checkForSpam(message) {
-    const spamWords = ['spam', 'sale'];
-    const lowerCasedMessage = message.toLowerCase();
-    for (let index = 0; index < spamWords.length; index++) {
-        if (lowerCasedMessage.includes(spamWords[index])) {
-            return true;
+'use strict'
+
+function filterArray(numbers, value) {
+    let resultArr = [];
+
+    for (const i of numbers) {
+        if (i > value) {
+            resultArr.push(i);
         }
     }
-    return false;
+    return resultArr;
 }
 
-console.log('Task 3');
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
-console.log('\n');
+console.log("Task 3");
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log("\n");
